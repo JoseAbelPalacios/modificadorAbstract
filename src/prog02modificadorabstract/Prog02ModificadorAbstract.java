22b43a6146680e1a906e97ed6bb858e50dd17a77
JoseAbelPalacios <joseabel_palacios@hotmail.com>
2016-11-11 17:22:51 -0600

    segundo commit desde netbeans

--- src/prog02modificadorabstract/Prog02ModificadorAbstract.java
+++ src/prog02modificadorabstract/Prog02ModificadorAbstract.java
@@ -15,7 +15,7 @@ public class Prog02ModificadorAbstract {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("lo cambie ");
     }
     
 }

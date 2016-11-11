2351a289406eef515d1d0a35bdfe42efb1d39067
JoseAbelPalacios <joseabel_palacios@hotmail.com>
2016-11-11 17:36:32 -0600

    probando conexion

--- src/prog02modificadorabstract/Prog02ModificadorAbstract.java
+++ src/prog02modificadorabstract/Prog02ModificadorAbstract.java
@@ -15,7 +15,7 @@ public class Prog02ModificadorAbstract {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("lo cambie ");
+        System.out.println("lo cambie gera noob");
     }
     
 }
